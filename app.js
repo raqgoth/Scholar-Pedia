5e2c8cad254c984bfe3de3ca9e23c1c1e4f2cc4d
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2020-10-19 14:13:44 -0400

    fix and delete extra code

--- app.js
+++ app.js
@@ -10,40 +10,19 @@ let words = [];
 search.addEventListener('click', (getWord) => {
     let searchString = search.target.value.toLowerCase();
 
-    let filteredWords = words.filter((word) => {
-        return (
-            words.toLowerCase().includes(searchString)
-        )})
-    
-    displayWords(filteredWords)
-        })
-
-
 const getWord = async () => {
     try {
         let response = await axios.get('https://www.dictionaryapi.com/api/v3/references/sd2/json/?key=37b7623c-c665-4cd7-b09c-422c6859cd77')
     let words = await response.data
-
-displayWords(words)
     }
     catch (error){
         console.log(error)
        
     }
 }
-let displayWords= (words) => {
-    const htmlString = words
-        .map((word) => {
-            return `
-<li class="word">
-                <h2>${word}</h2>
-                <p>${word}</p>
-                <img src="${word}"></img>
-            </li>
-        `;
-        })
-        .join('');
-        wordList.innerHTML = htmlString;
-};
-window.onload = getWord
+}
+)
+       
+
+getWord()
 

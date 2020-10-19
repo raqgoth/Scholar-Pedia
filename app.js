314c556cbd8e9d7ffe28cc32ddec235ff8b94fc7
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2020-10-19 19:44:35 -0400

    get search to work

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const API_KEY = '37b7623c-c665-4cd7-b09c-422c6859cd77'
 
 const WORD_URL = 'https://www.dictionaryapi.com/api/v3/references/sd2/json/test?key=37b7623c-c665-4cd7-b09c-422c6859cd77'
 
-let wordList = document.getElementById('wordList')
-let dogsDiv = document.querySelector('dogs')
+let buildResult = document.querySelector('.dictionary')
 let searchInput = document.getElementById('search-input')
 let searchButton = document.getElementById('search-button');
 console.log(searchInput, searchButton)
@@ -19,21 +18,26 @@ searchButton.addEventListener('click', (event) => {
 
     const getWord = async () => {
         try {
-            await axios.get(`https://www.dictionaryapi.com/api/v3/references/sd2/json/${searchWord}?key=37b7623c-c665-4cd7-b09c-422c6859cd77`)
-            .then(response => {
-                ("second")
-            })
-            console.log(words)
-            console.log(Object.values(response.data.target.value))
-            let resultWords = Object.values(response.data.target.value)
-            for (let i=0; i<resultWords.length; i++){
-                
-        }
-        catch (error){
-            console.log(error)
-       
-        }
+            const response = await axios.get(`https://www.dictionaryapi.com/api/v3/references/sd2/json/${searchWord}?key=37b7623c-c665-4cd7-b09c-422c6859cd77`)
+            buildResult(response.data)
+        } catch (error) {
+             console.log(error)
+        } 
     }
-    getWord()
+getWord() 
+
+  const buildResult = (data) => {
+    const dictionaryDiv = document.querySelector('.dictionary')
+    const result = document.createElement('div')
+        
+    result.forEach(data) 
+        let optionElement = document.createElement('option')
+        optionElement.innerText = `${taget.value}`
+        optionElement.setAttribute('value', symbol.symbol)
+        result.appendChild(optionElement)
+    }
+    
 })
+  
+
 

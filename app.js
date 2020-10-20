134f7f45aee9d7402a4b75d21409d9d14769c31a
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2020-10-19 21:56:47 -0400

    search appears on webpage

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const API_KEY = '37b7623c-c665-4cd7-b09c-422c6859cd77'
 
 const WORD_URL = 'https://www.dictionaryapi.com/api/v3/references/sd2/json/test?key=37b7623c-c665-4cd7-b09c-422c6859cd77'
-
 let buildResult = document.querySelector('.dictionary')
+
 let searchInput = document.getElementById('search-input')
 let searchButton = document.getElementById('search-button');
 console.log(searchInput, searchButton)
@@ -18,26 +18,24 @@ searchButton.addEventListener('click', (event) => {
 
     const getWord = async () => {
         try {
-            const response = await axios.get(`https://www.dictionaryapi.com/api/v3/references/sd2/json/${searchWord}?key=37b7623c-c665-4cd7-b09c-422c6859cd77`)
-            buildResult(response.data)
+            axios.get('https://www.dictionaryapi.com/api/v3/references/sd2/json/map?key=37b7623c-c665-4cd7-b09c-422c6859cd77')
+            .then((response)=>{
+                let wordStuff = response.data[0].shortdef
+                let dictionaryDiv = document.querySelector('.dictionary')
+                dictionaryDiv.textContent = wordStuff
+                console.log(response.data[0].shortdef)
+
+            })
+           
         } catch (error) {
              console.log(error)
         } 
     }
-getWord() 
-
-  const buildResult = (data) => {
-    const dictionaryDiv = document.querySelector('.dictionary')
-    const result = document.createElement('div')
-        
-    result.forEach(data) 
-        let optionElement = document.createElement('option')
-        optionElement.innerText = `${taget.value}`
-        optionElement.setAttribute('value', symbol.symbol)
-        result.appendChild(optionElement)
-    }
+    getWord() 
     
+    const result = document.createElement('div')
+    const buildResult = (data) => {
+        return result
+  }
+    buildResult()     
 })
-  
-
-

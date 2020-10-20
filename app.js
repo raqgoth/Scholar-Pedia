bec2f07f9208764b0786aa13742cf3dd8378ad6b
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2020-10-20 14:39:16 -0400

    add new search for synonims function

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const API_KEY = '37b7623c-c665-4cd7-b09c-422c6859cd77'
 
 const WORD_URL = (`https://www.dictionaryapi.com/api/v3/references/sd2/json/test?key=37b7623c-c665-4cd7-b09c-422c6859cd77`)
-let buildResult = document.querySelector('.dictionary')
+const SYN_URL = (`https://dictionaryapi.com/api/v3/references/thesaurus/json/test?key=b7b8c718-bbf6-49f6-a5f2-0e8a2a8f1c6b`)
 
+let buildResult = document.querySelector('.dictionary')
 let searchInput = document.getElementById('search-input')
 let searchButton = document.getElementById('search-button')
-console.log(searchInput, searchButton)
-let words = []
+let displayWord = document.querySelector('.word-search')
+let synonim = document.querySelector('.synonim')
+let synStuff = ""
+let synButton = document.getElementById('synSearch')
+console.log (searchInput, searchButton)
+let words = document.querySelector('.short-def')
 let searchWord = ""
 let wordStuff = ""
 let clearSearchButton = document.getElementById('clearSearch')
@@ -27,6 +32,9 @@ searchButton.addEventListener('click', (event) => {
                 let dictionaryDiv = document.querySelector('.dictionary')
                 dictionaryDiv.textContent = wordStuff
                 console.log(response.data[[0]].shortdef)
+                words.innerHTML = `${searchWord}`
+                displayWord.innerHTML = (response.data[[0]].shortdef)
+                
 
             })
            
@@ -45,10 +53,32 @@ searchButton.addEventListener('click', (event) => {
 })
 
 clearSearchButton.addEventListener('click', (event) => {
-document.getElementById('search-input').value = ''
-const CancelToken = axios.CancelToken;
-let wordStuff = CancelToken.source();
-const token = wordStuff.token;
-cancelToken: wordStuff.token
- }
+searchInput.value = ''
+searchWord = ''
+displayWord.innerHTML = ''
+words.innerHTML = ''
+synonim.innerHTML = ''
+})
+
+
+synButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    const getSyn = async () => {
+        try {
+            axios.get(`https://dictionaryapi.com/api/v3/references/thesaurus/json/${searchWord}?key=b7b8c718-bbf6-49f6-a5f2-0e8a2a8f1c6b`)
+            .then((response)=>{
+             
+                searchInput.addEventListener('change', function(event) {
+                    synStuff += event.syns.value
+                    console.log(synStuff)
+                 } )}
+            )}
+         catch (error) {
+             console.log(error)
+        } 
+       
+    getSyn()
+
+}}
 )
